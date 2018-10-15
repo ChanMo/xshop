3651078c45bd8a799edf510b4a612496936f4c6a
MacBook <chen@MacBook-Air.local>
2018-10-15 23:07:25 +0800

    add sort to commodity list, add modal to commodity

--- pages/commodity/commodity.js
+++ pages/commodity/commodity.js
@@ -3,6 +3,8 @@ const app = getApp()
 Page({
   data: {
     id: 0,
+    modalVisible: false,
+    modalAnimation: null,
     commodity: {}
   },
   onLoad: function(option) {
@@ -10,11 +12,36 @@ Page({
     this.setData({id:id})
     this._fetchCommodity()
   },
+  onShow: function() {
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+    })
+    animation.translateY(120)
+    this.setData({modalAnimation:animation.export()})
+  },
+  // 加入购物车
+  addToCart: function() {
+    wx.showToast({'title':'加入成功'})
+    this.closeModal()
+  },
+  // 获取商品信息
   _fetchCommodity: function() {
     let self = this
     let url = api.commodity + this.data.id + '/'
     wx.request({url, success:function(res){
       self.setData({commodity:res.data})
     }})
+  },
+  // 关闭modal
+  closeModal: function() {
+    this.setData({modalVisible:false})
+  },
+  // 开启modal
+  openModal: function() {
+    this.setData({modalVisible:true})
+  },
+  buy: function() {
+    wx.navigateTo({url:'/pages/buy/buy'})
   }
 })
--- pages/commodityList/commodityList.js
+++ pages/commodityList/commodityList.js
@@ -2,14 +2,26 @@ const api = require("../../api")
 
 Page({
   data: {
+    sort: 'default',
     data: []
   },
   onLoad: function() {
     this._fetchData()
   },
+  onPullDownRefresh: function() {
+    this._fetchData()
+    wx.stopPullDownRefresh()
+  },
+  // 设置排序方式
+  setSort: function(e) {
+    const value = e.currentTarget.dataset.value
+    this.setData({sort: value})
+    this._fetchData()
+  },
+  // 获取数据
   _fetchData: function() {
     let self = this
-    let url = api.commodity
+    let url = api.commodity + '?sort=' + this.data.sort
     wx.request({url, success:function(res){
       self.setData({data:res.data.results})
     }})
