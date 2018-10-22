22c1a6cbc2c5f5b8b241de05774bd703c0e6ea37
MacBook <chen@MacBook-Air.local>
2018-10-22 23:50:22 +0800

    add commodities

--- api.js
+++ api.js
@@ -2,12 +2,16 @@
 const domain = "http://wx.lcpxj.com/v1/"
 const api = {
   "login": domain + "user/login",
+  "sync": domain + "user/userInfo",
   "banner": domain + "index/ad",
   "link": domain + "index/navbar",
   "recommand": domain + "index/best",
+  "brand": domain + "category/brand",
   "category": domain + "category/lists",
   "commodities": domain + "goods/lists",
   "commodity": domain + "goods/detail",
   "page": "http://maybe.findchen.com/page/",
+  "addToCart": domain + "cart/add",
+  "cart": domain + "cart/lists"
 }
 module.exports = api
---
+++ pages/commodities/commodities.js
@@ -0,0 +1,11 @@
+const api = require('../../api')
+const app = getApp()
+
+Page({
+  data: {
+    commodities: []
+  },
+  onLoad: function(options) {
+    let category = options['category']
+  }
+})
