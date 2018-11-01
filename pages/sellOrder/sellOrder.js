39db3ce45cd184ac5a313e4160f67305f3fd85f3
MacBook <chen@MacBook-Air.local>
2018-11-01 23:18:35 +0800

    add sell order

---
+++ pages/sellOrder/sellOrder.js
@@ -0,0 +1,13 @@
+const app = getApp()
+
+const data = [
+  {'image':'http://imgs.nmplus.hk/wp-content/uploads/2015/12/零食_1.jpg','title':'手撕猪肉干','desc':'规格：50g','amount':20,'count':10},
+  {'image':'http://imgs.nmplus.hk/wp-content/uploads/2015/12/零食_1.jpg','title':'手撕猪肉干','desc':'规格：50g','amount':20,'count':10},
+  {'image':'http://imgs.nmplus.hk/wp-content/uploads/2015/12/零食_1.jpg','title':'手撕猪肉干','desc':'规格：50g','amount':20,'count':10}
+]
+
+Page({
+  data: {
+    data: data
+  }
+})
