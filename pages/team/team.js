afbd0e5aa1c24de59b633e463bda47a165682f00
MacBook <chen@MacBook-Air.local>
2018-11-01 23:18:49 +0800

    add team

---
+++ pages/team/team.js
@@ -0,0 +1,13 @@
+const app = getApp()
+
+const data = [
+  {'avatar':'http://imgtu.5011.net/uploads/content/20170605/4661271496665986.jpg','username':'May','count':10,'datetime':'2018-10-10 10:00'},
+  {'avatar':'http://imgtu.5011.net/uploads/content/20170630/2060641498789942.jpg','username':'Jone','count':20,'datetime':'2018-10-10 10:00'},
+  {'avatar':'http://imgtu.5011.net/uploads/content/20170711/9608901499761820.jpg','username':'Sande','count':12,'datetime':'2018-10-10 10:00'},
+]
+
+Page({
+  data: {
+    data: data
+  }
+})
