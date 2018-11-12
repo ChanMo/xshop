626845c0e2c815ddb7b3d6894862352cb3754573
MacBook <chen@MacBook-Air.local>
2018-11-12 23:05:54 +0800

    add api

--- pages/register/register.js
+++ pages/register/register.js
@@ -4,10 +4,22 @@ const app = getApp()
 Page({
   data: {
   },
-  onSubmit: function() {
+  onSubmit: function(e) {
     wx.showLoading({title:'申请中',mask:true})
-    setTimeout(()=>{
-      wx.showToast({title:'申请失败',icon:'cancel'})
-    }, 2000)
+    const url = api.register + '?token=' + app.globalData.token
+    if(!e.detail.value.name || !e.detail.value.mobile) {
+      wx.showToast({title:'请输入正确信息'})
+      return
+    }
+    wx.request({
+      url: url,
+      method: 'POST',
+      data: e.detail.value,
+      success: res=>{
+        wx.showToast({title:res.data.msg})
+      },
+      fail: error=>wx.showToast({title:'服务器失败'}),
+      complete: ()=>wx.hideLoading()
+    })
   }
 })
