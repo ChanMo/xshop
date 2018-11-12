5269bcd1bb4e1d381a3aa59457e44f5377028f91
MacBook <chen@MacBook-Air.local>
2018-11-12 23:06:07 +0800

    add api

--- pages/sellOrder/sellOrder.js
+++ pages/sellOrder/sellOrder.js
@@ -1,13 +1,15 @@
+const api = require('../../api')
 const app = getApp()
-
-const data = [
-  {'image':'http://imgs.nmplus.hk/wp-content/uploads/2015/12/零食_1.jpg','title':'手撕猪肉干','desc':'规格：50g','amount':20,'count':10},
-  {'image':'http://imgs.nmplus.hk/wp-content/uploads/2015/12/零食_1.jpg','title':'手撕猪肉干','desc':'规格：50g','amount':20,'count':10},
-  {'image':'http://imgs.nmplus.hk/wp-content/uploads/2015/12/零食_1.jpg','title':'手撕猪肉干','desc':'规格：50g','amount':20,'count':10}
-]
 
 Page({
   data: {
-    data: data
+    data: []
+  },
+  onLoad: function() {
+    this._fetchData()
+  },
+  _fetchData: function() {
+    const url = api.myorder + '?token=' + app.globalData.token
+    wx.request({url, success:res=>this.setData({data:res.data.data})})
   }
 })
