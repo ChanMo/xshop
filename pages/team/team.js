4802afe6a30b11c4a9b79c9ff26f5502044a6d10
MacBook <chen@MacBook-Air.local>
2018-11-12 23:06:20 +0800

    add api

--- pages/team/team.js
+++ pages/team/team.js
@@ -1,13 +1,15 @@
+const api = require('../../api')
 const app = getApp()
-
-const data = [
-  {'avatar':'http://imgtu.5011.net/uploads/content/20170605/4661271496665986.jpg','username':'May','count':10,'datetime':'2018-10-10 10:00'},
-  {'avatar':'http://imgtu.5011.net/uploads/content/20170630/2060641498789942.jpg','username':'Jone','count':20,'datetime':'2018-10-10 10:00'},
-  {'avatar':'http://imgtu.5011.net/uploads/content/20170711/9608901499761820.jpg','username':'Sande','count':12,'datetime':'2018-10-10 10:00'},
-]
 
 Page({
   data: {
-    data: data
+    data: []
+  },
+  onLoad: function() {
+    this._fetchData()
+  },
+  _fetchData: function() {
+    const url = api.team + '?token=' + app.globalData.token
+    wx.request({url, success:res=>this.setData({data: res.data.data})})
   }
 })
