0e0310035b45826e12abcfeec84e04fbfdb1bf7f
MacBook <chen@MacBook-Air.local>
2018-11-12 23:06:29 +0800

    add api

--- pages/walletLog/walletLog.js
+++ pages/walletLog/walletLog.js
@@ -1,14 +1,12 @@
 const api = require('../../api')
 const app = getApp()
 
-const data = [
-  {'title':'[普通用户]购买了某某某','amount':50,'datetime':'2018-10-10 12:00'},
-  {'title':'[一级用户]购买了某某','amount':50,'datetime':'2018-10-10 14:00'},
-  {'title':'提现','amount':-100,'datetime':'2018-10-12 12:00'},
-]
-
 Page({
   data: {
-    data: data
+    data: []
+  },
+  onLoad: function() {
+    const url = api.log + '?token=' + app.globalData.token
+    wx.request({url, success:res=>this.setData({data:res.data.data})})
   }
 })
