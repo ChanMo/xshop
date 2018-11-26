e65067d798b9ce459ade26a58279b727c3c801cf
MacBook <chen@MacBook-Air.local>
2018-11-26 11:33:49 +0800

    add qrcode, fixed refund for ticket

--- pages/order/order.js
+++ pages/order/order.js
@@ -20,6 +20,15 @@ Page({
       data: {'order_id':self.data.id},
       success: function(res) {
         self.setData({order:res.data.data.order})
+        if(res.data.data.order.virtual == 1) {
+          const drawQrcode = require('../../utils/weapp.qrcode.min.js')
+          drawQrcode({
+            width: 200,
+            height: 200,
+            canvasId: 'myQrcode',
+            text: res.data.data.order.order_no
+          })
+        }
       }
     })
   }
