f5db91d80e08068b5740c6c4f5eed37a62111bbd
MacBook <chen@MacBook-Air.local>
2018-12-25 09:40:17 +0800

    add time control, adjust styles

--- pages/ticket/ticket.js
+++ pages/ticket/ticket.js
@@ -1,12 +1,13 @@
 const api = require('../../api')
 const app = getApp()
-
+var WxParse = require('../../wxParse/wxParse.js');
 Page({
   data: {
     id: 0,
     ticket: null,
     modalVisible: false,
     count: 1,
+    content: '',
   },
   onLoad: function(option) {
     let id = option['id']
@@ -31,14 +32,18 @@ Page({
     let url = api.commodity + '?goods_id=' + this.data.id
     wx.request({url, success:function(res){
       if(res.data.code > 0) {
+        var content = res.data.data.detail.content.replace('<img', '<img style="max-width:100%;height:auto" ');
+        WxParse.wxParse('article', 'html', content, self,5);
         self.setData({
           commodity: res.data.data,
+          content: content,
           spec: res.data.data.detail.spec[0],
           spec_ids: res.data.data.detail.spec[0].spec_sku_id.split('_')
         })
       }
     }})
   },
+
 
   // 选择规格
   setSpec: function(e) {
@@ -56,7 +61,6 @@ Page({
     let result = this.data.commodity.detail.spec.filter(
       item => item.spec_sku_id == ids
     )
-    console.log(result)
     if(result.length > 0) {
       this.setData({spec: result[0]})
     }
@@ -85,6 +89,34 @@ Page({
     this.setData({count:this.data.count+1})
   },
   onBuy: function() {
-    wx.navigateTo({url:'/pages/buy/buy?commodity='+this.data.id+'&count='+this.data.count+'&sku='+this.data.spec.spec_sku_id+'&is_virtual=1'})
+    if(this.data.commodity.detail.grab == 20) {
+      wx.navigateTo({url:'/pages/buy/buy?commodity='+this.data.id+'&count='+this.data.count+'&sku='+this.data.spec.spec_sku_id+'&is_virtual=1'})
+      return
+    }
+    let start_time_str = this.data.commodity.detail.start_time
+    let start_time = new Date(
+      start_time_str.substr(0,4),
+      parseInt(start_time_str.substr(5,2)) - 1,
+      start_time_str.substr(8,2),
+      start_time_str.substr(11,2),
+      start_time_str.substr(14,2),
+      start_time_str.substr(17,2),
+    )
+    let end_time_str = this.data.commodity.detail.end_time
+    let end_time = new Date(
+      end_time_str.substr(0,4),
+      parseInt(end_time_str.substr(5,2)) - 1,
+      end_time_str.substr(8,2),
+      end_time_str.substr(11,2),
+      end_time_str.substr(14,2),
+      end_time_str.substr(17,2),
+    )
+    let now = new Date()
+    if(start_time <= now && now <= end_time) {
+      wx.navigateTo({url:'/pages/buy/buy?commodity='+this.data.id+'&count='+this.data.count+'&sku='+this.data.spec.spec_sku_id+'&is_virtual=1'})
+    } else {
+      wx.showToast({title:'抢购未开始'})
+      return
+    }
   }
 })
